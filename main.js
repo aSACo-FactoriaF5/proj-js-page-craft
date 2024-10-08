7840e6df0b0b98b73800327e9415e0b76e8df4b6
Cristian <cristian.a.osal@gmail.com>
2024-10-08 16:40:34 +0200

    end Epic 1.2

--- main.js
+++ main.js
@@ -7,13 +7,19 @@ const companyData = {
   imageUrl:
     "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
 };
+
 console.log(companyData);
 
+//señalar elementos
 const tituloDOM = document.getElementById("companyName")
+const imagenDOM = document.getElementById("companyImage")
 
 console.log(tituloDOM)
 
 //modificacion de atributos
-
 document.title = companyData.name
 tituloDOM.textContent = companyData.name      //tmb funciona amb innerHTML
+
+
+console.log(imagenDOM)
+imagenDOM.setAttribute("src", companyData.imageUrl)
